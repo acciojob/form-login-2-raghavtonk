087a2eebcbabc1114bb1e2b67e6f713ecebfd332
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-05 16:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",(e)=>{
+    let fname = document.getElementById("firstname").value;
+    let lname= document.getElementById("lastname").value;
+    let phno = document.getElementById("phonenumber").value;
+    let email = document.getElementById("email").value;
+    
+    alert(fname+" "+lname+" "+phno+" "+email)
+});
